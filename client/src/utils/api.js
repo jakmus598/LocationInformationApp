5ddd5a73a01570e71a4ff6f731df0e018d505518
Jake Musleh <jakmus598@gmail.com>
2020-01-15 15:00:53 -0600

    Updated API to account for development and production environments

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 var fetch = require('node-fetch')
 
-var API_URL = 'http://localhost:5000'
+//The URLs at which the API can be fetched
+const API_URL = process.env.NODE_ENV === 'production' ? 
+'http://location-wizard.herokuapp.com/events' : 'http://localhost:5000'
 
 export async function getEventInformation()
 {
-    var fetchRes = await fetch('http://localhost:5000/events')
+    var fetchRes = await fetch(API_URL + '/events')
     var eventJSON = await fetchRes.json()
     return eventJSON
     //res.json()).then(data => {return Promise.resolve(data)}))
