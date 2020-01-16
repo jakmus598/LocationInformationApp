b2d88ea19b8abb3c066ea877036e17ffa6b720e0
Jake Musleh <jakmus598@gmail.com>
2020-01-15 20:18:46 -0600

    Testing updates

--- index.js
+++ index.js
@@ -134,14 +134,14 @@ app.get('/events', async (req, res, error) => {
       //Get name, url to buy tickets, and date
       name = jsonRes['_embedded']['events'][i]['name']
       url = jsonRes['_embedded']['events'][i]['url']
-      //date = jsonRes['embedded']['events'][i]['dates']['start']['localDate']
-
+      date = jsonRes['embedded']['events'][i]['dates']['start']['localDate']
+      console.log(date)
       //Get time and convert it from military
       //time = jsonRes['embedded']['events'][i]['dates']['start']['localTime']
       //time = convertTime(time)
 
       //Store all information in a single Object
-      var basicInformation = {'name': name, 'url': url}
+      var basicInformation = {'name': name, 'url': url, 'date': date}
       //'date': date, 'time': time}
       eventInformation.push(basicInformation)
     }
