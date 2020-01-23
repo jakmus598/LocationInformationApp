478a0d89e79fac4263c63927308690156878fc8b
Jake Musleh <jakmus598@gmail.com>
2020-01-23 11:12:28 -0600

    Bug fixes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var sort = require('sort-algorithms')
 var mongoose = require('mongoose')
 require('./Models/User')
 var mongoDB = 'mongodb://127.0.0.1/LocationWizardUsers'
-mongoose.connect(keys.MONGO_URI, {useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
 
 //mongoose.connect(mongoDB, {useNewUrlParser: true})
 //console.log(mongoose.connection)
