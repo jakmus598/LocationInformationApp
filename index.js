fbf0f11c7cfd19dff9a1aec2673484217d6a164d
Jake Musleh <jakmus598@gmail.com>
2020-01-23 11:05:22 -0600

    Updated callback URLs

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ passport.use(new TwitterStrategy({
     accessToken: TWITTER_ACCESS_TOKEN,
     tokenSecret: TWITTER_TOKEN_SECRET,
     //callbackURL - Where user is redirected to after request is confirmed
-    callbackURL: '/login/twitter' 
+    callbackURL: '/'
 }, //(accessToken) => console.log(accessToken)))
 function(accessToken, tokenSecret, profile, done) {
     console.log(profile)
